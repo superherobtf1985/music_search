ec3ef420781c7fb085440a855b778d10829afaa8
shunto <s1610360@edu.cc.uec.ac.jp>
2019-11-12 19:10:43 +0900

    :tada: Initialized commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-title class="headline text-uppercase">
+        <router-link to="/" style="color: white; text-decoration: none;"><span>MUSIC SEARCH</span></router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        text
+        to="/"
+      >
+        <span class="mr-2">HOME</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-}
+  components: {
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
